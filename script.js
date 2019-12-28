779abbf4d7b4c9f1fd874143605cc3dc367a770a
Serhiy <oliynuk.serhiy@gmail.com>
2019-12-28 15:57:36 +0200

    first comit

---
+++ script.js
@@ -0,0 +1,48 @@
+
+var number = 5;
+var string = 'Hello';
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+
+//створим масив з переліком фоток
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+//вивід елемента масиву
+
+console.log(arr[2]);
+
+//alert("Hello World!");
+
+//let answer = confirm("Are you here?")
+
+//console.log(answer);
+
+let answer = prompt("Есть ли ам 18?", "Да");
+
+console.log(typeof(answer));  //перевірка на тип даних - typeof
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = true;
+    isClose = false;
+
+console.log(isChecked && isClose);
+
+//&&  -  I -  якщо 2 аргумента тру
+// || -  АБО  - або 1 або другий тру
+
+
+
+
+
